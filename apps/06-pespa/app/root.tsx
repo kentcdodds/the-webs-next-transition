5026cddbe7897bb78f3779235968177f62e99cdc
Kent C. Dodds <me+github@kentcdodds.com>
2022-09-21 04:39:49 +0200

    fix names

--- apps/06-pespa/app/root.tsx
+++ apps/06-pespa/app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
-	title: 'Remix TodoMVC',
+	title: 'PESPA TodoMVC',
 	viewport: 'width=device-width,initial-scale=1',
 })
 
